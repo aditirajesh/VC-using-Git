053e31f31ea9108a1cb5a7f1f7963060bde52f0a
Aditi Rajesh <aditir2607@gmail.com>
2025-03-19 14:36:25 +0530

    Adding file2_1 to master, ignoring textfile

---
+++ task2/file2_1.py
@@ -0,0 +1,6 @@
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+hobby = input("Enter hobby: ")
+
+sentence = f'hi my name is {name} I am {age} years old and my hobby is {hobby}'
+print(sentence)
