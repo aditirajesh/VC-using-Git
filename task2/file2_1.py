58f540cb921a5babc215b05d77edf4ede178012b
Aditi Rajesh <111845100+aditirajesh@users.noreply.github.com>
2025-03-19 14:37:23 +0530

    Delete task2 directory - had errors

--- task2/file2_1.py
+++
@@ -1,6 +0,0 @@
-name = input("Enter name: ")
-age = int(input("Enter age: "))
-hobby = input("Enter hobby: ")
-
-sentence = f'hi my name is {name} I am {age} years old and my hobby is {hobby}'
-print(sentence)
