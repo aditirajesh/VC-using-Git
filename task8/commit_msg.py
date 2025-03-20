13964faa503a7153204787a22c7500622f6f2231
Aditi Rajesh <aditir2607@gmail.com>
2025-03-20 15:11:57 +0530

    Test commit hook

--- task8/commit_msg.py
+++ task8/commit_msg.py
@@ -4,6 +4,7 @@ import re
 def main():
     print("Arguments are: ",str(sys.argv)) #prints out the arguments in the commit message
     print("Number of arguments are: ",len(sys.argv))
+    print("My name is aditi rajesh")
 
     sys.exit(1)
 
