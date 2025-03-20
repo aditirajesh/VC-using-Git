8246362abb67751ff6e331ce7bc73b321595744f
Aditi Rajesh <aditir2607@gmail.com>
2025-03-20 15:15:01 +0530

    testing round 2

--- task8/commit_msg.py
+++ task8/commit_msg.py
@@ -4,7 +4,6 @@ import re
 def main():
     print("Arguments are: ",str(sys.argv)) #prints out the arguments in the commit message
     print("Number of arguments are: ",len(sys.argv))
-    print("My name is aditi rajesh")
 
     sys.exit(1)
 
