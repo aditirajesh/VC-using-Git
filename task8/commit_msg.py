d293c51f21fd7d494fcbfbd75b974915ee9da021
Aditi Rajesh <aditir2607@gmail.com>
2025-03-20 15:24:50 +0530

    testing out commit hook

--- task8/commit_msg.py
+++ task8/commit_msg.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python3 
 import sys 
 import re
 
 def main():
     print("Arguments are: ",str(sys.argv)) #prints out the arguments in the commit message
     print("Number of arguments are: ",len(sys.argv))
+    print("My name is aditi rajesh")
 
-    sys.exit(1)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
